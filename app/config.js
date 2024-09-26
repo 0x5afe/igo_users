ad98f4f178f74a5caee8283d6f8cbf401c114688
Andry R <andry@archikoder.com>
2024-09-26 13:48:38 +0200

    initial commit: added .gitignore

---
+++ app/config.js
@@ -0,0 +1,5 @@
+
+module.exports.init = (config) => {
+  config.cookieSecret         = 'iqvmQBURXCq61oADerGcpjfUBwJDOZamWQeHUc2a';
+  config.cookieSession.keys   = [ 'vL01OectT553Pgp59cNjHKRCOaANDrXy5YaDrlvp' ];
+};
